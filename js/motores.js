0b80259043358d20ad36232f21ae66e7d44f53a8
Murilo Costa Gonçalves <murilo.costa@sga.pucminas.br>
2018-05-27 16:18:11 -0300

    Visualizar Motores

--- js/motores.js
+++ js/motores.js
@@ -1,4 +1,5 @@
 $("#container-msg-success").hide();
+$(document).ready(listarMotores);
 
 function adicionarMotor(numeropolos, tensaorede, regimeservico, correntenominal, potencianominal, torquemaximo){
     var motor = new Motor(numeropolos, tensaorede, regimeservico, correntenominal, potencianominal, torquemaximo);
@@ -6,9 +7,24 @@ function adicionarMotor(numeropolos, tensaorede, regimeservico, correntenominal,
         $("#msg-success").html("Motor inserido com sucesso");
         $("#container-msg-success").show();
         resetForm();
+        listarMotores();
     }
 }
 
 function resetForm(){
      document.getElementById("form-motor").reset(); 
+}
+
+function listarMotores(){
+    $("#content-table-motores").html("");
+    $MDB.tableMotor.forEach(function(motor){
+        var item = $("<tr></tr>");
+        item.append($("<td></td").text(motor.numeropolos));
+        item.append($("<td></td").text(motor.tensaorede));
+        item.append($("<td></td").text(motor.regimeservico));
+        item.append($("<td></td").text(motor.correntenominal));
+        item.append($("<td></td").text(motor.potencianominal));
+        item.append($("<td></td").text(motor.torquemaximo));
+        $("#content-table-motores").append(item);
+    });
 }
